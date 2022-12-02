a208869140d85fb8187bb620524da34afe76be22
shubhangimore <shubhamore0402@gmail.com>
2022-12-02 11:23:53 +0100

    setup file_added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='let_it_snow',
+    version='1.0.0',
+    url='https://github.com/ShubhangiMore0402/Assignmet_5',
+    packages= find_packages()
+)
